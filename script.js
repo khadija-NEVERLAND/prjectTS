1d3f0eb5a87d896a083bc61aa01f0f0e7c0c71af
khadija-NEVERLAND <elbachourikadija@gmail.com>
2024-05-25 11:25:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voices.forEach((voice, i) => {
+        voiceSelect.options[i] = new Option(voice.name, i);
+    });
+};
+
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
+});
+
+document.querySelector("button").addEventListener("click", () => {
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+});
+
+document.getElementById("click_to_convert").addEventListener('click', function () {
+    window.SpeechRecognition = window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+    recognition.interimResults = true;
+
+    recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
+            .map(result => result[0])
+            .map(result => result.transcript)
+            .join('');
+
+        document.getElementById("convert_to_text").innerHTML = transcript;
+    });
+
+    recognition.start();
+});
